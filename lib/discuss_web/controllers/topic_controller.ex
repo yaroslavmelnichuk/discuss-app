42a3898729eb5b8e2c7f4567b76b4bfafb12613a
Yaroslav Melnychuk <yariksuperman95@gmail.com>
2022-07-31 14:05:55 +0300

    Handle topics deletion

--- lib/discuss_web/controllers/topic_controller.ex
+++ lib/discuss_web/controllers/topic_controller.ex
@@ -17,12 +17,42 @@ defmodule DiscussWeb.TopicController do
   def create(conn, %{"topic" => topic}) do
     changeset = Topic.changeset(%Topic{}, topic)
     case Repo.insert(changeset) do
-      {:ok, post} ->
+      {:ok, _topic} ->
         conn
           |> put_flash(:info, "Topic created")
           |> redirect(to: Routes.topic_path(conn, :index))
       {:error, changeset} ->
         render(conn, "new.html", changeset: changeset)
     end
+  end
+
+  def edit(conn, %{ "id" => topic_id }) do
+    topic = Repo.get(Topic, topic_id)
+    changeset = Topic.changeset(topic)
+
+    render conn, "edit.html", changeset: changeset, topic: topic
+  end
+
+  def update(conn, %{ "id" => topic_id, "topic" => topic_params}) do
+    topic = Repo.get(Topic, topic_id)
+    changeset = Topic.changeset(topic, topic_params)
+
+    case Repo.update(changeset) do
+      {:ok, _topic} ->
+        conn
+          |> put_flash(:info, "Topic updated")
+          |> redirect(to: Routes.topic_path(conn, :index))
+      {:error, changeset} ->
+        render conn, "edit.html", changeset: changeset, topic: topic
+    end
+  end
+
+  def delete(conn, %{ "id" => topic_id }) do
+    Repo.get!(Topic, topic_id)
+      |> Repo.delete!
+
+      conn
+        |> put_flash(:info, "Topic deleted")
+        |> redirect(to: Routes.topic_path(conn, :index))
   end
 end
--- lib/discuss_web/router.ex
+++ lib/discuss_web/router.ex
@@ -18,9 +18,13 @@ defmodule DiscussWeb.Router do
     pipe_through :browser
 
     get "/", TopicController, :index
-    # get "/topics", TopicController, :index
-    get "/topics/new", TopicController, :new
-    post "/topics/create", TopicController, :create
+    # # get "/topics", TopicController, :index
+    # get "/topics/new", TopicController, :new
+    # post "/topics/create", TopicController, :create
+    # get "/topics/:id/edit", TopicController, :edit
+    # put "/topics/:id", TopicController, :update
+    # delete "/topics/:id", TopicController, :delete
+    resources "/topics", TopicController
   end
 
   # Other scopes may use custom stacks.
